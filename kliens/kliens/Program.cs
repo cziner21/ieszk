7e156b79b697842f68f6d235b82a6010be15fb6e
cziner21 <cziner.adam@gmail.com>
2014-03-16 17:26:01 +0100

    Szerver + kliens program erre a feladatra: http://aries.ektf.hu/~mnikolett/inteszk/SzerverKliens.txt

---
+++ kliens/kliens/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace kliens
+{
+    class Program
+    {
+        static int portSzam = 6666;
+        static string ipCim = "127.0.0.1";
+
+        static void Main(string[] args) {
+            try {
+                IPAddress ip = IPAddress.Parse(ipCim);
+                TcpClient client = new TcpClient();
+                Console.WriteLine("Kapcsolódás... {0}:{1}", ip.ToString(), portSzam);
+                client.Connect(ip, portSzam);
+                StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+                StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8);
+                string messageFromServer = reader.ReadLine();
+                Console.WriteLine(messageFromServer);
+
+                while (true) {
+                    Console.ResetColor();
+                    Console.WriteLine("Utasításra várok...");
+                    string utasitas = Console.ReadLine();
+                    writer.WriteLine(utasitas);
+                    writer.Flush();
+                    string response = reader.ReadLine();
+                    if (response == "OK") {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(response);
+                    }
+                    else if(response.StartsWith("ERR")){
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(response);
+                    }
+
+                }
+
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("Hiba lépett fel:\n {0}", e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ szerver/szerver/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace szerver
+{
+    class Operations
+    {
+        int a;
+        int b;
+        
+
+        int result;
+
+        public Operations(int a, int b) {
+            this.a = a;
+            this.b = b;
+            result = 0;
+        }
+
+        public void ADD() {
+            result = a + b;
+        }
+
+        public void DIV() {
+            result = a / b;
+        }
+
+        public void MULTIPLY() {
+            result = a * b;
+        }
+
+        public void SUBSTRACTION() {
+            result = a - b;
+        }
+
+        public void PRIMS() {
+
+        }
+
+        public void STOP() {
+
+        }
+    }
+
+    class Communication
+    {
+        public StreamReader r; 
+        public StreamWriter w; 
+
+
+        public Communication(TcpClient client) {
+            this.r = new StreamReader(client.GetStream(), Encoding.UTF8);
+            this.w = new StreamWriter(client.GetStream(), Encoding.UTF8);
+            result = 0;
+        }
+
+        public void StartCommunication() {
+            w.WriteLine("Üdvözöllek a szerveren!"+"Az szerver ezeket az utasításokat tudja:"
+                        +"ADD pl: ADD|5|6  (5+6)"
+                        +"DIV pl: DIV|9|3  (9/3)"
+                        +"MULTIPLY pl: MULTIPLY|2|2  (2*2)"
+                        + "SUBSTRACTION pl: SUBSTRACTION|6|3  (6-3)"
+                        +"PRIMS");
+            w.Flush();
+            while (true) {
+                string[] utasitas = null;
+                try {
+                    string keres = r.ReadLine();
+                    
+                    utasitas = keres.Split('|');
+                    a = int.Parse(utasitas[1]);
+                    b = int.Parse(utasitas[2]);
+                    switch (utasitas[0]) {
+                        case "ADD":
+                            ADD();
+                            break;
+                        case "MULTIPLY":
+                            MULTIPLY();
+                            break;
+                        case "SUBSTRACTION":
+                            SUBSTRACTION();
+                            break;
+                        case "DIV":
+                            DIV();
+                            break;
+                        case "PRIMS":
+                            PRIMS();
+                            break;
+                        case "BYE":
+                            STOP();
+                            break;
+                        default:
+                            w.WriteLine("ERR|Nem ismerem ezt a parancsot");
+                            break;
+                    }
+                }
+                catch (Exception e) {
+                    w.WriteLine("ERR|{0}", e.Message);
+                }
+                
+                w.Flush();
+                if (utasitas[0] == "BYE") break;
+            }
+        }
+
+
+        /*műveletek*/
+        public int a;
+        public int b;
+        
+
+        int result;
+
+        
+
+        public void ADD() {
+            result = a + b;
+            w.WriteLine("OK");
+        }
+
+        public void DIV() {
+            result = a / b;
+            w.WriteLine("OK");
+        }
+
+        public void MULTIPLY() {
+            result = a * b;
+            w.WriteLine("OK");
+        }
+
+        public void SUBSTRACTION() {
+            result = a - b;
+            w.WriteLine("OK");
+        }
+
+        public void PRIMS() {
+
+        }
+
+        public void STOP() {
+            w.WriteLine("A szerver befejezte a kommunikációt");
+        }
+    }
+
+    
+
+    
+
+    class Program
+    {
+        const int portSzam = 6666; // 1024 .. 65535 kozotti szam
+        const string ipCim = "127.0.0.1";
+        static void Main(string[] args) {
+            Console.WriteLine("Szerver indul...");
+            try {
+                IPAddress ip = IPAddress.Parse(ipCim);
+                TcpListener k = new TcpListener(ip, portSzam);
+                k.Start();
+                Console.WriteLine("A szerver sikeresen elindult {0}:{1}", ip.ToString(), portSzam);
+                while (true) {
+                    Console.WriteLine("A szerver a bejövő kapcsolatra vár.");
+                    TcpClient client = k.AcceptTcpClient();
+                    Console.WriteLine("bejövő kapcsolat megérkezett");
+                    Communication p = new Communication(client);
+                    Thread t = new Thread(p.StartCommunication);
+                    t.Start();  // p.startKomm();
+                }
+            }
+            catch {
+                Console.WriteLine("A szerver nem indult el, hiba miatt!");
+            }
+            Console.WriteLine("Kapcsolat vége, nyomj meg egy billentyűt a folytatáshoz...");
+            Console.ReadLine();
+        }
+    }
+}
